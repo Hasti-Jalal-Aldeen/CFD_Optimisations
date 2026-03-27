04d460ff72421fa6d768fa832c8fa633be087490
Hasti <hjaldeen@LAPTOP-SUJPBBVI.>
2026-03-27 16:08:04 +0000

    removed _LEGACY again

--- _LEGACY/Incompressible/ConvectionSchemes/Base.cpp
+++
@@ -1,9 +0,0 @@
-#include "Incompressible/ConvectionSchemes.hpp"
-
-ConvectionScheme::ConvectionScheme() {
-    return;
-}
-
-void ConvectionScheme::discretise_momentum(double* A, double* b[NDIMS], double* F, double* G[NDIMS], const Domain& domain) const {
-    return;
-}
